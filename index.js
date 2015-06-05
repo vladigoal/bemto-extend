1bcf914435dc6c7ec87acf3ab830a59b89e44948
Vladislav Gorbik <v.gorbik@doc.ua>
2015-06-05 18:18:17 +0300

    e

--- index.js
+++ index.js
@@ -12,32 +12,21 @@ module.exports = function() {
 
     function doReplace() {
 
-      console.log('lo')
       if (file.isBuffer()) {
           var chunks = String(file.contents).split(': +i');
-          var result;
+          var result = '';
 
-          
-          // console.log('chunks=', chunks.length)
           // result = chunks.join('\n' + blockSpaces + '    +te(' + blockName + ')');
-
-
-        console.log('chunks=', chunks)
-        console.log('============')
 
         for (var i = 0; i < chunks.length; i++){
             if(i < chunks.length - 1){
                 var block = chunks[i].split('\n')[chunks[i].split('\n').length - 1]
-                var blockSpaces = block.split('+b')[i]
+                var blockSpaces = block.split('+b')[0]
                 var blockName = block.split('.')[1]
-                console.log('chunks if=', chunks[i] + '\n' + blockSpaces + '    +te(' + blockName + ')')
-                result += chunks[i] + '\n' + blockSpaces + '    +te(' + blockName + ')';
+                result += chunks[i] + '\n' + blockSpaces + '    +i';
             }else{
-                console.log('chunk else ============')
                 result += chunks[i];
             }
-            // result += chunks[i] + '\n' + blockSpaces + '    +te(' + blockName + ')';
-          // result.push(replacement(search));
         }
 
           file.contents = new Buffer(result);
